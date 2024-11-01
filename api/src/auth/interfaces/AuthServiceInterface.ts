0760f3911a7a2cae905796acd290d15bafe0be9b
Ibrahim Elkhalil Benyahia <khalilbenyahia2@gmail.com>
2024-11-01 14:31:26 +0100

    updating interface

--- api/src/auth/interfaces/AuthServiceInterface.ts
+++ api/src/auth/interfaces/AuthServiceInterface.ts
@@ -1,8 +1,11 @@
 import { LocalUserLoginDto } from '../dto/LocalUserLoginDto';
 import { LocalUserRegisterDto } from '../dto/LocalUserRegisterDto';
 import { Response } from 'express';
+import { AuthReq } from '../guards/GeneralAuthenticator';
 
 export interface AuthServiceInterface {
     registerLocalUser(localUserRegisterDto:LocalUserRegisterDto): Promise<any>
     loginLocalUser(localUserLoginDto:LocalUserLoginDto,res:Response): Promise<any>
+    loginGoogleUSer(req:AuthReq, res:Response):Promise<void>
+    checkAuth(req:AuthReq):Promise<any>
 }
