8cf7293ba8f9fbf14299ace76d4f514d2217d44f
ibdev <khalilbenyahia2@gmail.com>
2024-11-04 14:36:24 -0500

    add the profile services to the controller

--- api/src/profile/profile.controller.ts
+++ api/src/profile/profile.controller.ts
@@ -1,17 +1,31 @@
-import { Controller,Delete,Get,Patch,UseGuards } from "@nestjs/common";
-import { GeneralAuthenticator } from "src/auth/guards/GeneralAuthenticator";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { GeneralAuthenticator } from 'src/auth/guards/GeneralAuthenticator';
+import { ProfileService } from './profile.service';
+import { User } from 'src/decorators/User';
+import { UpdateProfileDto } from './dto/UpdateProfileDto';
 
-@Controller("profile")
+@Controller('profile')
 @UseGuards(GeneralAuthenticator)
-export class ProfileController{
-    
+export class ProfileController {
+  constructor(private readonly profileService: ProfileService) {}
 
-    @Get()
-    getUserProfile(){}
+  @Get()
+  getUserProfile(@User('pid') profileId: number) {
+    return this.profileService.getUserProfile(profileId);
+  }
 
-    @Patch()
-    updateUserProfile(){}
-
-    @Delete()
-    deleteUserProfile(){}
+  @Patch()
+  updateUserProfile(
+    @User('pid') profileId: number,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    return this.profileService.updateUserProfile(profileId, updateProfileDto);
+  }
 }
